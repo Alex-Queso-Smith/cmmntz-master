9ca9189c0ebefce665920721551bf31ce78b8d4b
Alex Smith <alex@classibridge.com>
2018-10-15 16:32:06 -0400

    user form error handling

--- app/javascript/react/components/form/Input.js
+++ app/javascript/react/components/form/Input.js
@@ -4,7 +4,7 @@ const Input = props => {
   return(
     <div className="form-group">
       <label className="text-large" htmlFor={props.name}>{props.label}</label>
-      <input className="form-control margin-top-10px " type={props.type} name={props.name} value={props.content} onChange={props.handleChange}></input>
+      <input className={`form-control margin-top-10px ${props.addClass}`} type={props.type} name={props.name} value={props.content} onChange={props.handleChange}></input>
     </div>
   );
 };
--- app/javascript/react/components/form/UserRegPageOne.js
+++ app/javascript/react/components/form/UserRegPageOne.js
@@ -2,49 +2,77 @@ import React from 'react';
 
 import Input from './Input';
 
-const UserRegPageOne = props => {
-  return(
-    <div id="reg-required" className="form-group">
-      <h3 className="text-center">Sign-Up Required Information </h3>
-      <Input
-        name="userName"
-        label="User Name"
-        handleChange={props.handleChange}
-        content={props.userName}
-        type="text"
-      />
-      {props.userNameError}
-      <Input
-        name="email"
-        label="Email"
-        handleChange={props.handleChange}
-        content={props.email}
-        type="text"
-      />
-      {props.emailError}
-      <Input
-        name="password"
-        label="Password"
-        handleChange={props.handleChange}
-        content={props.password}
-        type="password"
-      />
-      {props.passwordError}
-      <Input
-        name="passwordConfirmation"
-        label="Password Confirmation"
-        handleChange={props.handleChange}
-        content={props.passwordConfirmation}
-        type="password"
-      />
-      {props.passwordConfirmationError}
-      <div className="form-group actions margin-top-10px">
-        <button id="user-registration-button-page-one" className="btn btn-block btn-large btn-dark" onClick={props.handleButtonClick} disabled={props.disabled}>
-          <span className="text-large">Next Page (Optional)</span>
-        </button>
+class UserRegPageOne extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+  render(){
+    var userNameClass, emailClass, passwordClass, passwordConfirmationClass;
+
+    if (this.props.userNameError) {
+      userNameClass = "is-invalid"
+    }
+
+    if (this.props.emailError) {
+      emailClass = "is-invalid"
+    }
+
+    if (this.props.passwordError) {
+      passwordClass = "is-invalid"
+    }
+
+    if (this.props.passwordConfirmationError) {
+      passwordConfirmationClass = "is-invalid"
+    }
+
+    return(
+      <div id="reg-required" className="form-group">
+        <h3 className="text-center">Sign-Up Required Information </h3>
+        <Input
+          name="userName"
+          label="User Name"
+          handleChange={this.props.handleChange}
+          content={this.props.userName}
+          type="text"
+          addClass={userNameClass}
+        />
+        {this.props.userNameError}
+        <Input
+          name="email"
+          label="Email"
+          handleChange={this.props.handleChange}
+          content={this.props.email}
+          type="text"
+          addClass={emailClass}
+        />
+        {this.props.emailError}
+        <Input
+          name="password"
+          label="Password"
+          handleChange={this.props.handleChange}
+          content={this.props.password}
+          type="password"
+          addClass={passwordClass}
+        />
+        {this.props.passwordError}
+        <Input
+          name="passwordConfirmation"
+          label="Password Confirmation"
+          handleChange={this.props.handleChange}
+          content={this.props.passwordConfirmation}
+          type="password"
+          addClass={passwordConfirmationClass}
+        />
+        {this.props.passwordConfirmationError}
+        <div className="form-group actions margin-top-10px">
+          <button id="user-registration-button-page-one" className="btn btn-block btn-large btn-dark" onClick={this.props.handleButtonClick} disabled={this.props.disabled}>
+            <span className="text-large">Next Page (Optional)</span>
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default UserRegPageOne;
