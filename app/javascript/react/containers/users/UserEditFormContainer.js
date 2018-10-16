328b744b008d6e9b9c582f25866b174cd362e9c0
Matthew Wright <matt@classibridge.com>
2018-10-16 11:20:41 -0400

    updadted userEditForm

--- app/javascript/react/containers/users/UserEditFormContainer.js
+++ app/javascript/react/containers/users/UserEditFormContainer.js
@@ -33,6 +33,31 @@ class UserEditFormContainer extends React.Component {
     this.validateErrorKeys = this.validateErrorKeys.bind(this);
   }
 
+  componentDidMount(){
+    fetch(`/api/v1/users/${this.props.match.params.id}.json`, {credentials: 'same-origin'})
+    .then(response => {
+       if(response.ok){
+         return response
+       } else {
+         let errorMessage = `${response.status} (${response.statusText})`,
+             error = new Error(errorMessage)
+         throw(error)
+       }
+     })
+     .then(response => response.json())
+     .then(body => {
+       this.setState({
+         userName: body.user_name,
+         email: body.email,
+         ageRange: body.age_range,
+         gender: body.gender,
+         latitude: body.latitude,
+         longitude: body.longitude 
+       })
+     })
+     .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
   handleChange(event){
     if (
       event.target.name != "ageRange" ||
