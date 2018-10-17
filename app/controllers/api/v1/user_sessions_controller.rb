e458a5e891c4eed11882c2d02d32d6962aadd4d3
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-10-17 11:40:53 -0400

    allow remember_me to pass through

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -24,6 +24,6 @@ class Api::V1::UserSessionsController < ApplicationController
   private
 
   def user_session_params
-    params.require(:user_session).permit(:user_name, :password)
+    params.require(:user_session).permit(:user_name, :password, :remember_me)
   end
 end
