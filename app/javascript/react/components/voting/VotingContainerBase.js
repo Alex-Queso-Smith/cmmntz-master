d5ce7fb9aae918c1cf471e72463c521827e74738
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-10-22 15:30:08 -0400

    cycle over an array to produce, set group (for use in future), tmp pass toggle state trhough array

--- app/javascript/react/components/voting/VotingContainerBase.js
+++ app/javascript/react/components/voting/VotingContainerBase.js
@@ -2,23 +2,24 @@ import React from 'react';
 import VoteButton from './VoteButton'
 
 const VotingContainerBase = props => {
+
+  const voteTypes = [["like", "Like", "exclusive", "btn toggled"], ["indifferent", "Indifferent", "exclusive", ""], ["dislike", "Dislike", "exclusive", ""]]
+
+  var voteButtons = voteTypes.map((type) => {
+    return(
+      <VoteButton
+        key={type[0]}
+        name={type[0]}
+        label={type[1]}
+        group={type[2]}
+        toggleClass={type[3]}
+      />
+    )
+  })
+
   return(
     <div className="cf-votes-container margin-top-10px" >
-      <VoteButton
-        name="like"
-        display="Like"
-        toggleClass="btn toggled"
-      />
-      <VoteButton
-        name="indifferent"
-        display="Indifferent"
-        toggleClass=""
-      />
-      <VoteButton
-        name="dislike"
-        display="Dislike"
-        toggleClass=""
-      />
+      {voteButtons}
     </div>
   );
 };
