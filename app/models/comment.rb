30ff747362e1535f73a7486593f6cc4a2d95361a
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-10-26 13:22:29 -0400

    set to use list of votes on create comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 include ActionView::Helpers::SanitizeHelper
 class Comment < ApplicationRecord
+  attr_accessor :vote_types
+
   belongs_to :user
   has_many :votes
-  accepts_nested_attributes_for :votes
   has_many :comment_interactions
 
   before_validation :sanitize_text
@@ -11,6 +12,8 @@ class Comment < ApplicationRecord
 
   validates :text, length: { in: 1..3000 }
   validate :text_does_not_have_html
+
+  after_create_commit :parse_and_create_votes
 
   private
 
@@ -21,10 +24,20 @@ class Comment < ApplicationRecord
     errors[:text] << "HTML is not allowed in text" if strip_tags(text_hold) != text
   end
 
-
   ### before_validations actions
 
   def sanitize_text
     self.text = sanitize(text, tags: []) unless text.blank?
+  end
+
+  ### Postprocessors
+
+  def parse_and_create_votes
+    # "type1,type2..typex"
+    vote_list = []
+    vote_types.split(',').each do |vote|
+      v = self.votes.build(user_id: user_id, vote_type: vote)
+      vote_list << v if v.valid?
+    end
   end
 end
