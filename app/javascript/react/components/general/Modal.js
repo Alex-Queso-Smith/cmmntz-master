75b2fb9d9ac118de2f9b0ab25ff03cfb1415ff51
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-01 14:21:08 -0400

    no longer need show/hide classes/css as the render is state based

--- app/javascript/react/components/general/Modal.js
+++ app/javascript/react/components/general/Modal.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
 const Modal = props => {
-  var showHideClassName = props.show ? "cf-modal cf-display-block" : "cf-modal cf-display-none";
-
   return (
-    <div className={showHideClassName}>
+    <div className="cf-modal">
       <section className="cf-modal-main">
         <img src="/assets/interface/modal/modal-close.png" onClick={props.handleClose} className="cf-modal-close" />
         <h3>{props.modalTitle}</h3>
