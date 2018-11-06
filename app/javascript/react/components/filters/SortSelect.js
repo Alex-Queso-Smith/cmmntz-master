0d1bb9d0e6388ecde5498610f7a531dcf48d4cb5
Jesse <jesse@Jesses-MacBook-Pro.local>
2018-11-06 14:44:01 -0500

    Merge branch 'master' of github.com:Classibridge/classifilter-master

--- app/javascript/react/components/filters/SortSelect.js
+++ app/javascript/react/components/filters/SortSelect.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const SortDir = props => {
   return(
-    <div className="col-sm">
+    <div className="col-1 col-sm-1 col-md-1">
       <img className={`sort-btn cursor-pointer margin-top-10px vote-btn sort-${props.name}`} onClick={props.onClick} name="sortDir" data-value={props.value} src={props.image} />
     </div>
   );
@@ -10,7 +10,7 @@ export const SortDir = props => {
 
 export const SortButton = props => {
   return(
-    <div className="col-sm">
+    <div className="col-1 col-sm-1 col-md-1">
       <img className={`sort-btn cursor-pointer margin-top-10px vote-btn sort-${props.name} ${props.visibility} ${props.opacity}`} onClick={props.onClick} name="sortType" data-value={props.value} src={props.image} />
     </div>
   )
