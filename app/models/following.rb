0e879f1b2f4fff6f80e30f6e7a8ebd802cee2774
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-07 13:43:16 -0500

    basic validation specs

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,4 @@
 class Following < ApplicationRecord
-  belongs_to :follower, class: :user
-  belongs_to :following, class: :user
+  belongs_to :follower, class_name: 'User', foreign_key: :follower_id
+  belongs_to :following, class_name: 'User', foreign_key: :following_id
 end
--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Following, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let!(:follow) { FactoryBot.build(:following) }
+
+    it "should be valid with follower and following" do
+      expect(follow).to be_valid
+    end
+
+    it "should be invalid without follower" do
+      follow.follower = nil
+      expect(follow).to_not be_valid
+    end
+
+    it "should be invalid without following" do
+      follow.following = nil
+      expect(follow).to_not be_valid
+    end
+
+  end
 end
