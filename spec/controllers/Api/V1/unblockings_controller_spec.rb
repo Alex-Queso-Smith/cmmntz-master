87c6b87e7ca6fde446ce8e5fc219a711ef21bf18
Alex Smith <alex@classibridge.com>
2018-11-12 10:37:31 -0500

    unblockings controller tests

---
+++ spec/controllers/Api/V1/unblockings_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UnblockingsController, type: :controller do
+  describe 'while authenticated do' do
+    setup :activate_authlogic
+    before(:each) do
+      @user = FactoryBot.create(:user)
+      @other_user = FactoryBot.create(:user)
+    end
+
+    # This should return the minimal set of attributes required to find a valid
+    # Blocking. As you add validations to Blocking, be sure to
+    # adjust the attributes here as well.
+    let(:valid_attributes) {
+      FactoryBot.attributes_for(:blocking, blocker_id: @user.id, blocking_id: @other_user.id)
+    }
+
+    let(:invalid_attributes) {
+      FactoryBot.attributes_for(:blocking, blocker_id: @user.id, blocking_id: nil)
+    }
+
+    # This should return the minimal set of values that should be in the session
+    # in order to pass any filters (e.g. authentication) defined in
+    # UnblockingsController. Be sure to keep this updated too.
+    let(:valid_session) { {} }
+
+    describe "POST #create" do
+      context "with valid params" do
+        it "finds and destroys the correct blocking" do
+          Blocking.create(valid_attributes)
+            expect {
+              post :create, format: :json, params: { blocking: valid_attributes }, session: valid_session
+            }.to change(Blocking, :count).by(-1)
+        end
+
+        it "responds successfully" do
+          post :create, format: :json, params: { blocking: valid_attributes }, session: valid_session
+          expect(response).to be_successful
+        end
+
+        context "with invalid params" do
+          it "returns a success response (i.e. to display the 'new' template)" do
+            post :create, format: :json, params: { blocking: invalid_attributes }, session: valid_session
+            expect(response). to be_successful
+          end
+        end
+      end
+    end
+  end
+end
