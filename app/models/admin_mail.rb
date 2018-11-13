ab8f5d9e078be7145c49ae5d4a85e1ae57a86f8e
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-13 13:54:13 -0500

    striping of html from subject and content

--- app/models/admin_mail.rb
+++ app/models/admin_mail.rb
@@ -1,11 +1,21 @@
+include ActionView::Helpers::SanitizeHelper
 class AdminMail < ApplicationRecord
   belongs_to :user
 
+  before_validation :strip_html_from_content_and_subject
+
   validates :user_id, :subject, :content, presence: true
+  validates :content, length: { in: 1..1000 }
+  validates :subject, length: { in: 1..100 }
 
   after_create :send_mail
 
   private
+
+  def strip_html_from_content_and_subject
+    self.subject = strip_tags(subject)
+    self.content = strip_tags(content)
+  end
 
   def send_mail
     to = user.email
--- spec/models/admin_mail_spec.rb
+++ spec/models/admin_mail_spec.rb
@@ -22,11 +22,51 @@ RSpec.describe AdminMail, type: :model do
       admin_mail.content = nil
       expect(admin_mail).to_not be_valid
     end
+
+    describe "subject validations" do
+      it "should not allow subject greater than 100 characters" do
+        admin_mail.subject = "Vivamus eget urna accumsan, lobortis velit ac, porta ante. Ut ac nunc luctus, bibendum sapien in, consequat mauris. Nullam lobortis posuere gravida. Quisque efficitur ligula eget ex dapibus congue. Proin gravida gravida ante et tincidunt. Aliquam posuere dignissim pellentesque. Aliquam erat volutpat. Curabitur facilisis est eget facilisis accumsan. Sed consequat urna lorem, eu tincidunt risus tempor eget. Duis sit amet ipsum in velit fringilla efficitur id non lorem. Nam vitae ligula rutrum, molestie lacus in, rhoncus ipsum."
+        expect(admin_mail).to_not be_valid
+      end
+
+      it "must have at least 1 character" do
+        admin_mail.subject = ""
+        expect(admin_mail).to_not be_valid
+      end
+
+      it "should sanitize from tags" do
+        admin_mail.subject = "<p>text<p>"
+        text_sanitized = "text"
+        admin_mail.valid?
+        expect(admin_mail.subject).to eq(text_sanitized)
+      end
+    end
+
+    describe "content validations" do
+      it "should not allow content greater than 1000 characters" do
+        admin_mail.content = "Vivamus eget urna accumsan, lobortis velit ac, porta ante. Ut ac nunc luctus, bibendum sapien in, consequat mauris. Nullam lobortis posuere gravida. Quisque efficitur ligula eget ex dapibus congue. Proin gravida gravida ante et tincidunt. Aliquam posuere dignissim pellentesque. Aliquam erat volutpat. Curabitur facilisis est eget facilisis accumsan. Sed consequat urna lorem, eu tincidunt risus tempor eget. Duis sit amet ipsum in velit fringilla efficitur id non lorem. Nam vitae ligula rutrum, molestie lacus in, rhoncus ipsum.
+
+        Aenean scelerisque, odio ut viverra iaculis, nunc magna placerat sem, eget viverra massa urna id lorem. Vivamus dignissim felis lacus, eu cursus ex congue at. Vivamus vitae dolor dui. Cras volutpat ante dolor, id facilisis erat lacinia id. Ut rhoncus nunc vel erat feugiat, ut blandit nibh euismod. Pellentesque orci eros, posuere ut tristique ac, mattis quis justo. Nunc finibus ipsum nec est euismod interdum. Vivamus imperdiet ipsum nec pulvinar venenatis. Fusce sed feugiat nisi, eget auctor sapien. Sed eget ipsum nec urna porttitor tristique ac nec mi. Praesent porttitor pellentesque eleifend."
+        expect(admin_mail).to_not be_valid
+      end
+
+      it "must have at least 1 character" do
+        admin_mail.content = ""
+        expect(admin_mail).to_not be_valid
+      end
+
+      it "should sanitize from tags" do
+        admin_mail.content = "<p>text<p>"
+        text_sanitized = "text"
+        admin_mail.valid?
+        expect(admin_mail.content).to eq(text_sanitized)
+      end
+    end
   end
 
   describe "callbacks" do
     let!(:admin_mail) { FactoryBot.build(:admin_mail) }
-    
+
     it "should send an email after create" do
       expect { admin_mail.save }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
