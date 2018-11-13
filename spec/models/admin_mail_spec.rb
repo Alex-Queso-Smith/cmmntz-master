57b0ff568d0ce764a8c6da3bb62ac87e08e9e03a
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-13 13:41:21 -0500

    basic testing for admin mailer

--- spec/models/admin_mail_spec.rb
+++ spec/models/admin_mail_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe AdminMail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let!(:admin_mail) { FactoryBot.build(:admin_mail) }
+
+    it "should be valid with valid attributes" do
+      expect(admin_mail).to be_valid
+    end
+
+    it "should be invalid without a user_id" do
+      admin_mail.user = nil
+      expect(admin_mail).to_not be_valid
+    end
+
+    it "should be invalid without a subject" do
+      admin_mail.subject = nil
+      expect(admin_mail).to_not be_valid
+    end
+
+    it "should be invalid witout content" do
+      admin_mail.content = nil
+      expect(admin_mail).to_not be_valid
+    end
+  end
+
+  describe "callbacks" do
+    let!(:admin_mail) { FactoryBot.build(:admin_mail) }
+    
+    it "should send an email after create" do
+      expect { admin_mail.save }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
 end
