d8117eee9ff27013cf4f436f823484f9b59692e3
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-16 11:46:29 -0500

    adjust stay and block pos for smaller screens

--- app/javascript/react/components/comments/Reply.js
+++ app/javascript/react/components/comments/Reply.js
@@ -77,19 +77,19 @@ class Reply extends React.Component {
         blockOpacity = "translucent"
       }
       followStar =
-      <div className={`col-1 col-sm-1 cursor-pointer ${starOpacity}`}>
+      <div className={`col-2 col-sm-2 col-md-1 cursor-pointer ${starOpacity}`}>
         <img onClick={this.handleFollow} src="/assets/star" height="20px" width="20px" />
       </div>
       blockSym =
-      <div className={`col-1 col-sm-1 cursor-pointer ${blockOpacity}`}>
+      <div className={`col-2 col-sm-2 col-md-1 cursor-pointer ${blockOpacity}`}>
         <img onClick={this.handleBlock} src="/assets/block" height="20px" width="20px" />
       </div>
     } else {
       blockSym =
-      <div className={`col-1 col-sm-1`}>
+      <div className={`col-2 col-sm-2 col-md-1`}>
       </div>
       followStar =
-      <div className={`cf-comment-user-name col-1 col-sm-1`}>
+      <div className={`cf-comment-user-name col-2 col-sm-2 col-md-1`}>
       </div>
     }
 
@@ -105,7 +105,7 @@ class Reply extends React.Component {
             />
           <div className="cf-comment-w-meta">
             <div className="cf-comment-comment-meta row">
-              <div className="cf-comment-user-name col-2 float-left">
+              <div className="cf-comment-user-name col-4 col-sm-4 col-md-2">
                 {user_name}
               </div>
               {followStar}
