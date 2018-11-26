0f1f707a4de1383aa8f0f4d2fac91e7fc85a2f6b
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-26 14:42:34 -0500

    save

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
 class Article < ApplicationRecord
+  include Fae::BaseModelConcern
+  def fae_display_field
+    
+  end
+
+  include Fae::BaseModelConcern
+  def fae_display_field
+    
+  end
+  has_fae_file :banner
+
+  def fae_display_field
+    title
+  end
+
   def url(request = {})
     if Rails.env.production?
       base_url = "classifilter-master.herokuapp.com"
