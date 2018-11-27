2a7c2584f4e24d57ec401f046db12cbe5c7b53f0
Alex Smith <alex@classibridge.com>
2018-11-27 16:15:19 -0500

    showing censored comments finish up

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -14,4 +14,8 @@ module CommentsHelper
   def comment_user_base_image(comment)
     comment.render_anonymously? ? "" : comment.user.base_image
   end
+
+  def comment_user_show_censored(comment)
+    comment.render_anonymously? ? true : comment.user.show_censored_comments
+  end
 end
