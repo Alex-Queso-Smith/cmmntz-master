9f2b7347aaaaff74df3920627dc2ad1d8610458e
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-28 10:12:34 -0500

    use homebrew slug gen to have date and title

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,8 @@ class Article < ApplicationRecord
   belongs_to :author
   has_fae_file :banner
 
+  before_validation :generate_slug!, on: :create
+
   validates :title, :publish_date, :body, :introduction, presence: true
   validates :slug, Fae.validation_helpers.slug
 
@@ -21,5 +23,12 @@ class Article < ApplicationRecord
       base_url = "#{request.base_url.gsub("http://", "")}"
     end
     "#{base_url}/#{self.class.to_s.pluralize.downcase}/#{slug}"
+  end
+
+  private
+
+  def generate_slug!
+    return unless publish_date && title
+    self.slug = "#{publish_date.strftime("%Y-%m-%d")}-#{title.downcase.gsub(/[^0-9A-Za-z\s]/, '').gsub(" ", "-")}"
   end
 end
