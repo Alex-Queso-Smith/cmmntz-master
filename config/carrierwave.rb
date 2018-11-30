2a869205e0cc419c987bf67620417e8e3ae9e67f
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-11-30 11:48:56 -0500

    install carrierwave directly and add azure hookup gem

---
+++ config/carrierwave.rb
@@ -0,0 +1,7 @@
+# CarrierWave.configure do |config|
+#   config.azure_storage_account_name = 'YOUR STORAGE ACCOUNT NAME'
+#   config.azure_storage_access_key = 'YOUR STORAGE ACCESS KEY'
+##   config.azure_storage_blob_host = 'YOUR STORAGE BLOB HOST' # optional
+#   config.azure_container = Rails.env
+##   config.asset_host = 'YOUR CDN HOST' # optional
+# end
