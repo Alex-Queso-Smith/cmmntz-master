4593e5a0c5e84cbe195d2259d27a3c9d0acfeb34
Alex Smith <alex@classibridge.com>
2018-12-11 16:59:51 -0500

    avatar images

--- app/javascript/react/containers/comments/UserAvatar.js
+++ app/javascript/react/containers/comments/UserAvatar.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import { AgeRangeImageSelector } from '../../components/general/General';
 
 class UserAvatar extends React.Component {
   state = {
@@ -8,10 +10,39 @@ class UserAvatar extends React.Component {
 
   render(){
     var { user_name, gender, age_range, base_image } = this.props.userInfo
+
     var { followStar, blockSym } = this.props
     var avatarImage = '/assets/avatar-default';
+
     if (base_image) {
       avatarImage = `/assets/${base_image}`
+    }
+
+    var ageRangeImage;
+    if (age_range != "") {
+      const style = {
+        height: "50px",
+        width: "50px"
+      }
+      if (age_range === "15-19") {
+        age_range = "13-19"
+      }
+      ageRangeImage =
+      <div className="col-sm-6">
+        <img style={style} src={`/assets/age-ranges/${age_range}`} />
+      </div>
+    }
+
+    var genderImage;
+    if (gender != "") {
+      const style = {
+        height: "50px",
+        width: "50px"
+      }
+      genderImage =
+      <div className="col-sm-6">
+        <img style={style} src={`/assets/genders/gender-${gender}`} />
+      </div>
     }
 
     var infoTile;
@@ -31,11 +62,9 @@ class UserAvatar extends React.Component {
               <div className="col-sm-10" />
             </div>
           </div>
-          <div className="text-center margin-top-10px">
-            {gender}
-          </div>
-          <div className="text-center margin-top-10px">
-            {age_range}
+          <div className="row margin-top-10px gender-image-box">
+            {genderImage}
+            {ageRangeImage}
           </div>
         </div>
       </div>
