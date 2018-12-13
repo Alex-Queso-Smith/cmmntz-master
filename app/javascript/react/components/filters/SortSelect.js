e70eca5eea4ae84aa21c93100136a5571f3bc895
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-13 15:46:53 -0500

    adding style

--- app/javascript/react/components/filters/SortSelect.js
+++ app/javascript/react/components/filters/SortSelect.js
@@ -10,8 +10,9 @@ export const SortDir = props => {
 
 export const SortButton = props => {
   return(
-    <div className={`col-1 col-sm-1 col-md-1 vote-button-box ${props.className}`}>
-      <img title={`${props.title}`} className={`sort-btn cursor-pointer margin-top-10px vote-btn sort-${props.name} ${props.visibility} ${props.opacity}`} onClick={props.onClick} name="sortType" data-value={props.value} src={props.image} />
+    <div className={`col-1 col-sm-1 col-md-1 vote-button-box tooltip-container ${props.className}`}>
+      <img className={`sort-btn cursor-pointer margin-top-10px vote-btn sort-${props.name} ${props.visibility} ${props.opacity}`} onClick={props.onClick} name="sortType" data-value={props.value} src={props.image} />
+      <span className="tooltip-content">{`${props.title}`}</span>
     </div>
   )
 };
