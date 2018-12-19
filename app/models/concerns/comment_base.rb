38ade0cbe6552b356199bb7a1bde33604448b16b
Jesse <jesse@Jesses-MacBook-Pro.local>
2018-12-19 12:53:09 -0500

    finishing this up. When a warn vote is cast it will check to ensure that the comment has not dropped below the acceptability thresholds. if it has it will reneg its approval unless it has been perviously approved by a mod.

--- app/models/concerns/comment_base.rb
+++ app/models/concerns/comment_base.rb
@@ -8,18 +8,18 @@ module CommentBase
     anonymous? || user.nil? || user.guest?
   end
 
-  private
 
   def auto_flag_moderator_activate!
     # do not proceed if a mod has already allowed this comment
-    return if approved_by == "mod"
+    return true if approved_by == "mod"
     flagged_votes = flag_votes.size
     # do not proceed unless the comment has more than MIN_FLAG_COUNT
-    return unless flagged_votes >= MIN_FLAG_COUNT
+    return true unless flagged_votes >= MIN_FLAG_COUNT
     # do not proceed unless the comment's flagged_percent is >= MIN_FLAG_PERCENT
-    return unless (flagged_votes.to_f / comment_interactions.size.to_f) >= MIN_FLAG_PERCENT
+    return true unless (flagged_votes.to_f / comment_interactions.size.to_f) >= MIN_FLAG_PERCENT.to_f
     # if it has reached this point, force the comment into moderation
     self.update_columns(approved: false, approved_by: nil)
+    return true
   end
 
 end
