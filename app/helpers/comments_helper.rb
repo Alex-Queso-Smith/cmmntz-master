cd1a7f467a9d6741d5f5e3993b507b62a1f055d8
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-20 15:40:59 -0500

    mark and display comments as having beein submitted by guest

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,21 +1,21 @@
 module CommentsHelper
   def comment_user_user_name(comment)
-    comment.render_anonymously? ? "Anonymous" : comment.user.user_name
+    comment.render_anonymously? ? "Anonymous" : comment.guest? ? "Guest" : comment.user.user_name
   end
 
   def comment_user_gender(comment)
-    comment.render_anonymously? ? "" : comment.user.gender_display
+    comment.render_anonymously? ? "" : comment.guest? ? "" : comment.user.gender_display
   end
 
   def comment_user_age_range(comment)
-    comment.render_anonymously? ? "" : comment.user.age_range_display
+    comment.render_anonymously? ? "" : comment.guest? ? "" : comment.user.age_range_display
   end
 
   def comment_user_base_image(comment)
-    comment.render_anonymously? ? "" : comment.user.base_image
+    comment.render_anonymously? ? "" : comment.guest? ? "" : comment.user.base_image
   end
 
   def comment_user_show_censored(comment)
-    comment.render_anonymously? ? true : comment.user.show_censored_comments
+    comment.render_anonymously? ? true : comment.guest? ? true : comment.user.show_censored_comments
   end
 end
