56bd6fa1b96953247b8e780ed7a6f10376609ad3
Jesse <jesse@Jesses-MBP.fios-router.home>
2018-12-20 10:31:23 -0500

    shorten human readable time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
     renderer    = Redcarpet::Render::HTML.new(options)
     @markdown ||= Redcarpet::Markdown.new(renderer, extensions)
     @markdown.render(text).html_safe
-  end    
+  end
 
   def page_title(title = "")
     t = "Classibridge Times"
@@ -33,7 +33,7 @@ module ApplicationHelper
   def display_time_ago timestamp
     return "" if timestamp.blank?
     return display_date_mm_yy(timestamp) if 3.months.ago > timestamp
-    (minutes_in_words(timestamp) + " ago").html_safe
+    (minutes_in_words(timestamp)).html_safe
   end
 
   def minutes_in_words(timestamp)
@@ -42,19 +42,19 @@ module ApplicationHelper
     return "" if minutes < 0
 
     case minutes
-    when 0..4            then 'less than 5 minutes'
-    when 5..14           then 'less than 15 minutes'
-    when 15..29          then 'less than 30 minutes'
-    when 30..59          then 'about 30 minutes'
-    when 60..119         then 'about 1 hour'
-    when 120..239        then 'about 2 hours'
-    when 240..479        then 'about 4 hours'
-    when 480..719        then 'about 8 hours'
-    when 720..1439       then 'about 12 hours'
-    when 1440..11519     then 'about ' << pluralize((minutes/1440).floor, 'day')
-    when 11520..43199    then 'about ' << pluralize((minutes/11520).floor, 'week')
-    when 43200..525599   then 'about ' << pluralize((minutes/43200).floor, 'month')
-    else                      'about ' << pluralize((minutes/525600).floor, 'year')
+    when 0..4            then '5 minutes'
+    when 5..14           then '15 minutes'
+    when 15..29          then '30 minutes'
+    when 30..59          then '30 minutes'
+    when 60..119         then '1 hour'
+    when 120..239        then '2 hours'
+    when 240..479        then '4 hours'
+    when 480..719        then '8 hours'
+    when 720..1439       then '12 hours'
+    when 1440..11519     then '' << pluralize((minutes/1440).floor, 'day')
+    when 11520..43199    then '' << pluralize((minutes/11520).floor, 'week')
+    when 43200..525599   then '' << pluralize((minutes/43200).floor, 'month')
+    else                      pluralize((minutes/525600).floor, 'year')
     end
   end
 
