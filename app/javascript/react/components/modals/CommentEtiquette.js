af88dfa6e872569d30fa65c56db58ebac3dde627
Alex Smith <alex@classibridge.com>
2019-01-07 10:25:30 -0500

    Typo fix

--- app/javascript/react/components/modals/CommentEtiquette.js
+++ app/javascript/react/components/modals/CommentEtiquette.js
@@ -12,9 +12,9 @@ export const CommentEtiquette = props => {
     <div className="cf-commenting-etiquette-body">
       <p>Please be respectful of others when commenting, including refraining from threatening, bullying or harassing as well as commenting for the sake of trolling.</p>
       <p>Spamming is frowned upon and could result in a ban as well as deletion of all comments.</p>
-      <p>Impersonating other individual is not allowed, nor is publishing personal contact information.</p>
+      <p>Impersonating other individuals is not allowed, nor is publishing personal contact information.</p>
       <p>Illegal or sexually suggestive material, including pornography, is not allowed and could result in being banned.</p>
-      <p>Inciting violence is also prohibited and is strictly enforced.Please use this platform to have productive discussions and enjoy yourself and please be mindful of the rights of others as well.</p>
+      <p>Inciting violence is also prohibited and is strictly enforced. Please use this platform to have productive discussions and enjoy yourself and please be mindful of the rights of others as well.</p>
     </div>
   }
 
