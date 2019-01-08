8c58dd44a899ade40d317359bdb1698a82c3dd8a
Jesse <jesse@Jesses-MBP.fios-router.home>
2019-01-08 14:21:18 -0500

    style the system authorize page

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,11 @@ module ClassifilterMaster
     # setting app to eastern time by default
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :utc
+
+    # do not allow rails to apply its own error handling divs
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      html_tag
+    }
   end
 
 end
