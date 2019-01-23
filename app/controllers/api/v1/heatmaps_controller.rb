c9c0c23876b4c1919301310125f4aa6e5d0b3d82
Jesse <jesse@Jesses-MBP.fios-router.home>
2019-01-23 16:45:55 -0500

    eliminate null lat longs

--- app/controllers/api/v1/heatmaps_controller.rb
+++ app/controllers/api/v1/heatmaps_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::HeatmapsController < ApiController
   def index
     comments = Comment.complete_listing_for_thread_heatmap(params[:art_id], current_user)
-    @users = comments.map(&:user).flatten.reject{ |u| u.blank? }
+    @users = comments.map(&:user).flatten.reject{ |u| u.blank? || u.latitude.blank? || u.longitude.blank? }
   end
 end
