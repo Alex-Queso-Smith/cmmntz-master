fe82f10847cccb707aac07d46a2daa381bdadb6a
Jesse <jesse@Jesses-MBP.fios-router.home>
2019-01-31 11:14:34 -0500

    look page restyled a bit

--- app/javascript/react/components/form/UserThemeSelector.js
+++ app/javascript/react/components/form/UserThemeSelector.js
@@ -10,25 +10,25 @@ const UserThemeSelector = props => {
   return(
     <div className="user-theme-selector">
 
-      <div id="cf-theme-selector-options-wrapper">
-        <div id="cf-theme-selector-options-left">
+      <div className="row" id="cf-theme-selector-options-wrapper">
+        <div className="col-6" id="cf-theme-selector-options-left">
           <label style={leftStyle} className="cf-text-medium">
             Select Theme:
             <br />
             <br />
-            <select name="colorTheme" value={props.colorTheme} onChange={props.onChange}>
+            <select name="colorTheme" value={props.colorTheme} onChange={props.onChange} className="form-control">
               <option value="cf-light">Light</option>
               <option value="cf-dark">Dark</option>
             </select>
           </label>
         </div>
 
-        <div id="cf-theme-selector-options-right">
+        <div className="col-6" id="cf-theme-selector-options-right">
           <label style={rightStyle} className="cf-text-medium">
           Select Font:
           <br />
           <br />
-          <select name="font" value={props.font} onChange={props.onChange}>
+          <select name="font" value={props.font} onChange={props.onChange} className="form-control">
             <option value="cf-serif" className="cf-serif">Serif</option>
             <option value="cf-sans-serif" className="cf-sans-serif">Sans-Serif</option>
           </select>
