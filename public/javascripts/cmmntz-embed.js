2da2aa8a2f95a74c115dfb28806ee1cfaa78beb7
Jesse <jesse@Jesses-MBP.fios-router.home>
2019-02-05 17:20:24 -0500

    try pointing directly at master through heroku

--- public/javascripts/cmmntz-embed.js
+++ public/javascripts/cmmntz-embed.js
@@ -112,7 +112,8 @@ var Cmmntz = Cmmntz || (function(){
           if (mode) {
             return mode == "staging" ? "https://classifilter-staging.herokuapp.com" : "http://localhost:3000"
           } else {
-            return "https://api.cmmntz.com"
+            // return "https://api.cmmntz.com"
+            return "https://classifilter-master.herokuapp.com"
           }
         }
     };
