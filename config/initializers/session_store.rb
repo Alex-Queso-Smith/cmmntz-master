f37b264eea952c86118e0a3946aa724eaaece0cf
Jesse <jesse@Jesses-MacBook-Pro.local>
2019-02-05 18:50:30 -0500

    try this

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-CmmntzMaster::Application.config.session_store :cookie_store, key: '_cmmntz_widget_session', secure: Rails.env.production?
+CmmntzMaster::Application.config.session_store :cookie_store, key: '_cmmntz_widget_session_new'#, secure: Rails.env.production?
